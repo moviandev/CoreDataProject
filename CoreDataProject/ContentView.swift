3eac2288d51eefa656407000844f5b458a9d0f7d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-10 23:43:41 -0300

    Add student struct and making it conform to hashble to use \.self

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+    
     var body: some View {
-        List {
-            // when using \.self we saying the whole object is identifiable
-            ForEach([2,4,6,8,10], id: \.self) {
-                Text("\($0) is even")
-            }
+        List(students, id: \.self) { student in
+            Text(student.name)
         }
     }
 }
