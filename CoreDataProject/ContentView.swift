8bc88c5dcb1a97c53a51f9fb1e6af0279db74dee
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-11 15:32:43 -0300

    Rollback

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -10,34 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH %@", "E")) var ships: FetchedResults<Ship>
     
     var body: some View {
-        VStack {
-            List(ships, id: \.self) { ship in
-                Text(ship.name ?? "Unknown name")
-            }
-            
-            Button("Add Examples") {
-                let ship1 = Ship(context: moc)
-                ship1.name = "Enterprise"
-                ship1.universe = "Star trek"
-                
-                let ship2 = Ship(context: moc)
-                ship2.name = "Defiant"
-                ship2.universe = "Star trek"
-                
-                let ship3 = Ship(context: moc)
-                ship3.name = "Millenium Falcon"
-                ship3.universe = "Star Wars"
-                
-                let ship4 = Ship(context: moc)
-                ship4.name = "Executor"
-                ship4.universe = "Star Wars"
-                
-                try? moc.save()
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
