0390d3dd3c5bca68c0591371de894445564163f5
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-11 15:16:16 -0300

    rollback

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -10,27 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
-    
     var body: some View {
-        VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
-            }
-            
-            Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
