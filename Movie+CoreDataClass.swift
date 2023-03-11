c7944070510a1c00b8e4e108733ef61f6fbc8371
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-10 23:50:46 -0300

    Change our codegen to be Manual/None, and created a NS Subclass adding with our group file

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Matheus Viana on 10/03/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
