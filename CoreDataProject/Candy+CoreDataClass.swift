93a4f71caffedabd662cbb20af531c1c8a391389
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-11 16:25:35 -0300

    Create subclasses

---
+++ CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Matheus Viana on 11/03/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Matheus Viana on 11/03/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
