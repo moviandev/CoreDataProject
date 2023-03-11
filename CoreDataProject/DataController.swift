d5236409c4c5678337d469fde9a8c3611fd1a904
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-10 23:31:05 -0300

    Create dataController

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Matheus Viana on 10/03/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
