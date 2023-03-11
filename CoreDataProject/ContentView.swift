c2a7b73ffa3d7185be28bdcf184d706c67df4a76
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-11 15:25:59 -0300

    NSPredicate to filter the values we want to be shown in our list

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -5,14 +5,39 @@
 //  Created by Matheus Viana on 10/03/23.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == '%@'", "Star Wars")) var ships: FetchedResults<Ship>
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millenium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
+            }
+        }
     }
 }
 
