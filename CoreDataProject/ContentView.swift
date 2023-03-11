4d9f6b3cda6ece932de8ff131fb2c34198b47a26
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-10 23:45:18 -0300

    Rollback

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,17 +7,10 @@
 
 import SwiftUI
 
-struct Student: Hashable {
-    let name: String
-}
-
 struct ContentView: View {
-    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
-    
     var body: some View {
-        List(students, id: \.self) { student in
-            Text(student.name)
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
