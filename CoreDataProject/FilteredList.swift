91c7c6f73047f2b90b0a7ad206207fa9035b421d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-11 16:12:57 -0300

    Creating a generic view to add filter

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -5,18 +5,21 @@
 //  Created by Matheus Viana on 11/03/23.
 //
 
+import CoreData
 import SwiftUI
 
-struct FilteredList: View {
-    @FetchRequest var fetchRequest: FetchedResults<Singer>
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+    let content: (T) -> Content
     
     var body: some View {
-        List(fetchRequest, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        List(fetchRequest, id: \.self) { item in
+            self.content(item)
         }
     }
     
-    init(filter: String) {
-        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
