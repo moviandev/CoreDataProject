ce92821a469cc7b9c130bc0de2370468580710e6
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-11 15:52:06 -0300

    Create buttons to add examples and change the letter we're filtering

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -10,10 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
+    @State private var lastNameFilter = "A"
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            // list of matching singers
+            
+            Button("Add Examples") {
+                let taylor = Singer(context: moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
+                
+                let ed = Singer(context: moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
+                
+                let adele = Singer(context: moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
+                
+                try? moc.save()
+            }
+            
+            Button("Show A") {
+                lastNameFilter = "A"
+            }
+            
+            Button("Show S") {
+                lastNameFilter = "S"
+            }
+        }
     }
 }
 
