92e0810291a29850c9278b21cb63e25072fb8b97
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-10 23:32:47 -0300

    Instantiate CoreData into CoreDataProejct.swift

--- CoreDataProject/DataController.swift
+++ CoreDataProject/DataController.swift
@@ -9,7 +9,7 @@ import CoreData
 import Foundation
 
 class DataController: ObservableObject {
-    let container = NSPersistentContainer(name: "Bookworm")
+    let container = NSPersistentContainer(name: "CoreDataProject")
     
     init() {
         container.loadPersistentStores { description, error in
